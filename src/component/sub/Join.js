9e0d23584225311da0704aaaf0dc57d91d6b9dca
Lee haemun <lhmun1129@daum.net>
2022-06-05 16:35:48 +0900

    id,pwd 에러 적용

--- src/component/sub/Join.js
+++ src/component/sub/Join.js
@@ -1,9 +1,89 @@
 import Layout from '../common/Layout';
-
+import { useState, useEffect } from 'react';
 function Join() {
+	const initVal = {
+		userid: '',
+		email: '',
+	};
+	const [Val, setVal] = useState(initVal);
+	const [Err, setErr] = useState({});
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		console.log('name', name);
+		console.log('value', value);
+		setVal({ ...Val, [name]: value });
+	};
+
+	const check = (Val) => {
+		const errs = {};
+
+		if (Val.userid.length < 5) {
+			errs.userid = '아이디를 5글자이상 입력하세요.';
+		}
+
+		if (Val.email.length < 8 || !/@/.test(Val.email)) {
+			errs.email = '이메일을 8글자이상 @를 포함해주세요.';
+		}
+		return errs;
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setErr(check(Val));
+	};
+
+	useEffect(() => {
+		console.log(Err);
+	}, [Err]);
 	return (
 		<Layout name={'Join'}>
-			<p>Join</p>
+			<form onSubmit={handleSubmit}>
+				<fieldset>
+					<legend>회원가입 양식</legend>
+					<table border='1'>
+						<caption>회원가입 정보입력</caption>
+						<tbody>
+							<tr>
+								<th scope='row'>
+									<label htmlFor='useid'>USER ID</label>
+								</th>
+								<td>
+									<input
+										type='text'
+										name='userid'
+										id='userid'
+										placeholder='아이디를 입력하세요'
+										value={Val.userid}
+										onChange={handleChange}
+									/>
+								</td>
+							</tr>
+							<tr>
+								<th scope='row'>
+									<label htmlFor='email'>E-MAIL</label>
+								</th>
+								<td>
+									<input
+										type='text'
+										name='email'
+										id='email'
+										placeholder='이메일 주소를 입력하세요'
+										value={Val.email}
+										onChange={handleChange}
+									/>
+								</td>
+							</tr>
+							<tr>
+								<th colSpan='2'>
+									<input type='reset' value='CANCEL' />
+									<input type='submit' value='SUBMIT' />
+								</th>
+							</tr>
+						</tbody>
+					</table>
+				</fieldset>
+			</form>
 		</Layout>
 	);
 }
--- src/component/sub/Location.js
+++ src/component/sub/Location.js
@@ -40,6 +40,7 @@ function Location() {
 	};
 
 	useEffect(() => {
+		container.current.innerHTML = '';
 		const map_instance = new kakao.maps.Map(container.current, options);
 
 		const markerPosition = Info[Index].latLng;
@@ -61,8 +62,29 @@ function Location() {
 		marker.setMap(map_instance);
 		setLocation(map_instance);
 
-		window.addEventListener('resize', () => {
+		//스카이뷰 전환버튼 추가
+		const mapTypeControl = new kakao.maps.MapTypeControl();
+		map_instance.addControl(
+			mapTypeControl,
+			kakao.maps.ControlPosition.TOPLEFT
+		);
+
+		// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+		const zoomControl = new kakao.maps.ZoomControl();
+		map_instance.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+		const mapCenter = () => {
+			console.log('함수호출');
 			map_instance.setCenter(Info[Index].latLng);
+		};
+
+		window.addEventListener('resize', () => {
+			mapCenter();
+			return () => {
+				window.removeEventListener('resize', () => {
+					mapCenter();
+				});
+			};
 		});
 	}, [Index]);
 
